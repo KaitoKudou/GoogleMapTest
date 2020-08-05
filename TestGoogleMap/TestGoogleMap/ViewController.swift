9ed4944f9e1c4cabb17aa8194694bfbf7caa1dc0
KaitoKudou <kaitokudou2468110921@gmail.com>
2020-08-06 03:24:03 +0900

    タップした箇所にピンを表示するようにした

--- TestGoogleMap/TestGoogleMap/ViewController.swift
+++ TestGoogleMap/TestGoogleMap/ViewController.swift
@@ -9,24 +9,27 @@
 import UIKit
 import GoogleMaps
 
-class ViewController: UIViewController, CLLocationManagerDelegate {
+class ViewController: UIViewController, CLLocationManagerDelegate, GMSMapViewDelegate {
     
     var mapView = GMSMapView()
     let locationManager = CLLocationManager() // GoogleMapsをインポートしているためか、CoreLocationをインポートしなくてもCLLocationManagerオブジェクトを生成できる
+    var currentLocation: CLLocation?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
         showMap()
         requestLocation()
+        mapView.delegate = self
     }
 
     func showMap(){
         // GoogleMapの初期位置(とりま東京駅)
         let camera = GMSCameraPosition.camera(withLatitude: 35.6812226, longitude: 139.7670594, zoom: 12.0)
         mapView = GMSMapView.map(withFrame: CGRect(x: 0, y: 0, width: 0, height: 0), camera: camera)
-        mapView.isMyLocationEnabled = true
+        mapView.isMyLocationEnabled = true // 現在地を表示する
         self.view = mapView
     }
     
@@ -54,6 +57,36 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         let longitude = location?.coordinate.longitude
         print("緯度：\(String(describing: latitude))")
         print("緯度：\(String(describing: longitude))")
+        
+        let camera = GMSCameraPosition.camera(withLatitude: latitude!, longitude: longitude!,zoom: 12.0)
+        mapView = GMSMapView.map(withFrame: CGRect(x: 0, y: 0, width: 0, height: 0), camera: camera)
+        mapView.isMyLocationEnabled = true // 現在地を表示する
+        
+        if mapView.isHidden {
+          mapView.isHidden = false
+          mapView.camera = camera
+        } else {
+          mapView.animate(to: camera)
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        <#code#>
+    }
+    
+    // マップをタップした時に呼ばれるdelegateメソッド
+    func mapView(_ mapView: GMSMapView, didTapPOIWithPlaceID placeID: String, name: String, location: CLLocationCoordinate2D) {
+        
+        mapView.clear() // 一旦全てのマーカーを消すことで，地図上に表示されるマーカーを1つだけにする
+        
+        print("You tapped \(name): \(placeID), \(location.latitude)/\(location.longitude)")
+        print("---------------------------------------------------------------")
+        
+        let marker = GMSMarker() // マーカーのインスタンス
+        marker.position = CLLocationCoordinate2D(latitude: location.latitude, longitude: location.longitude) //タップしたときの座標をピンの座標に指定
+        marker.title = name
+        marker.appearAnimation = .pop // ピンを立てる時にアニメーションを追加する
+        marker.map = mapView
     }
     
 }
